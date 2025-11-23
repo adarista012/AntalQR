429ea99c1e24c521697511ea3c529b9c1fc5babf
adarista012 <ad.arista012@gmail.com>
2025-11-23 16:48:40 -0400

    Initial Commit

---
+++ AntalQR/AntalQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  AntalQRApp.swift
+//  AntalQR
+//
+//  Created by antonio dante arista rivas on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct AntalQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AntalQR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AntalQR
+//
+//  Created by antonio dante arista rivas on 11/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
