c36d6f01d82ad7d01b17eed78e95e47182635141
adarista012 <ad.arista012@gmail.com>
2025-12-04 00:11:56 -0400

    first commit

---
+++ AntalQR/Core/DesingSystem/DesingSystem.swift
@@ -0,0 +1,31 @@
+//
+//  DesingSystem.swift
+//  AntalQR
+//
+//  Created by antonio dante arista rivas on 11/23/25.
+//
+
+import SwiftUI
+
+struct Spacing {
+    static let xs: CGFloat = 4
+    static let sm: CGFloat = 8
+    static let md: CGFloat = 16
+    static let lg: CGFloat = 20
+    static let xl: CGFloat = 32
+}
+
+struct CornerRadius {
+    static let sm: CGFloat = 6
+    static let md: CGFloat = 10
+    static let lg: CGFloat = 16
+    static let xl: CGFloat = 28
+    static let xxl: CGFloat = 44 
+}
+
+struct Sizes {
+    static let small : CGFloat = 150
+    static let medium : CGFloat = 200
+    static let large : CGFloat = 250
+    static let extraLarge : CGFloat = 300
+}
---
+++ AntalQR/Features/QREditor/QREditorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  QrEditorViewModel.swift
+//  AntalQR
+//
+//  Created by antonio dante arista rivas on 12/3/25.
+//
+import Foundation
+import SwiftUI
+
+@Observable
+final class QrEditorViewModel {
+    
+}
